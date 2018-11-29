607109059710994951bea9a0e43ba59e91e10024
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-11-28 22:46:03 -0800

    initial server setup

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const router = require("exopress").Router();
+
+router.get("/gitrepos", (req, res) => {
+  axios
+    .get("https://api.github.com/search/repositories?", { params: req.params })
+    .then(({ data: { results } }) => res.json(results))
+    .catch(err => res.status(422).json(err));
+});
+
+module.export = router;
